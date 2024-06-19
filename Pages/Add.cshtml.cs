b718b4b7fe5bfa783d9a75106427c2aa0aaf0f6f
Alexandru Mihai <115073416+AlexMihai1126@users.noreply.github.com>
2024-06-19 18:50:28 +0300

    CRUD zboruri complet

--- Pages/Add.cshtml.cs
+++ Pages/Add.cshtml.cs
@@ -1,47 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
 using ModelExamen.ContextModels;
 using ModelExamen.Models;
 using System.Drawing;
 
 namespace ModelExamen.Pages
 {
- 
-    public class EditModel : PageModel
+    public class AddModel : PageModel
     {
         [BindProperty]
         public Zbor Zbor { get; set; }
         public List<SelectListItem> Piloti { get; set; }
         public ProiectDBContext _dbContext { get; set; }
-        public EditModel(ProiectDBContext dbContext)
+        public AddModel(ProiectDBContext dbContext)
         {
             _dbContext = dbContext;
             Piloti = _dbContext.Piloti.Select(pilot => new SelectListItem { Text = pilot.Nume, Value = pilot.Id.ToString() }).ToList();
         }
-
-        public IActionResult OnGet(int zborId)
+        public void OnGet()
         {
             Zbor = new Zbor();
-            Zbor = _dbContext.Zboruri.Include(zbor => zbor.Pilot).FirstOrDefault(zbor => zbor.ZborId == zborId);
-            if(Zbor != null)
-            {
-                return Page();
-            }
-            else
-            {
-                return RedirectToPage("/Error");
-            }
         }
-
         public IActionResult OnPost()
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-            _dbContext.Zboruri.Update(Zbor);
+            _dbContext.Zboruri.Add(Zbor);
             _dbContext.SaveChanges();
             return RedirectToPage("/Index");
         }
--- Pages/Delete.cshtml.cs
+++ Pages/Delete.cshtml.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ModelExamen.ContextModels;
+using ModelExamen.Models;
 
 namespace ModelExamen.Pages
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Zbor Zbor { get; set; }
+        private readonly ProiectDBContext _dbContext;
+
+        public DeleteModel(ProiectDBContext dbContext)
         {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult OnGet(int zborId)
+        {
+            Zbor = _dbContext.Zboruri.Include(zbor => zbor.Pilot).FirstOrDefault(zbor => zbor.ZborId == zborId);
+            if (Zbor == null)
+            {
+                return RedirectToPage("/Error");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int zborId)
+        {
+            var zborToDelete = _dbContext.Zboruri.Find(zborId);
+            if (zborToDelete == null)
+            {
+                return RedirectToPage("/Error");
+            }
+
+            _dbContext.Zboruri.Remove(zborToDelete);
+            _dbContext.SaveChanges();
+            return RedirectToPage("/Index");
         }
     }
 }
